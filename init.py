ec0d6b717a8c169c4b6923b77cb3be965c854bca
thomastieyi <897253127@qq.com>
2019-03-11 13:11:41 +0800

    second commit

--- init.py
+++ init.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-   使用turtle库画一个动画渐变类的多边形
-   @aurthor: Tony@USTB
-   @Github: https://github.com/
-
-
-
+   @Description:使用turtle库画一个动画渐变类的多边形
+   @Aurthor: Tony@USTB
+   @Github: https://github.com/thomastieyi/turtlePaints
+   @Required_ENV: Pythin-2.7
 """
 import turtle
 # 引入Turtle库
@@ -21,21 +19,38 @@ colors = ["red", "yellow", "green", "blue", "orange", "purple"]
 # 渐变颜色数组
 
 def draw(sides,angle):
-    t.setheading(angle)
+    """
+       @Description:绘制单个颜色渐变的多变形，可以根据输入选择来绘制不同的形状.
+       @Input: {int} sides (0<sides<6) 绘制边数; {int} angle (0<angle<360) 绘制初始角度.
+       @Output: Graphs.
+    """
+    t.setheading(angle)   
+    # 设置启示角度
     for x in range(200):
+        # 循环次数代表这多边形的饱满程度
         t.tracer(False)
+        # 隐藏画图轨迹
         t.pencolor(colors[x % sides])
+        # 设置渐变的颜色
         t.forward(x * 3 / sides + x)
         t.left(360 / sides + 1)
         t.width(x * sides / 200)
+        # 绘制
         t.tracer(True)
  
    
 
 def main():
+    """
+       @Description:利用for循环一次绘制颜色渐变的多变形.
+       @Input: None.
+       @Output: Graphs.
+    """
     j=0
     t.hideturtle()
+    # 隐藏乌龟
     for i in range(0,360,15):
+        # 画图
         t.tracer(False)
         t.penup()
         t.home()
---
+++ readme.md
@@ -0,0 +1,20 @@
+# 基于`turtle`绘制的颜色渐变的多边形动画
+
+
+### 设计目标：
+* 利用turtle绘制动画来掌握python的函数定义方法、语句结构以及注释方法
+* 完成一个可以动的的多边形，欣赏几何美感
+
+### 设计思路：
+1. 利用`for`循环不断画出多边形的边，在每次开始绘画前调整角度与线的颜色，此时完成一个多变形绘制
+2. 在`main`函数中不断调用`1`中函数，完成不同多变形绘制，并在每次初始阶段赋予不同的启示角度以达到旋转的视觉效果
+
+### 运行结果：
+
+![result](https://meiri-img.oss-cn-beijing.aliyuncs.com/sss.gif)
+
+
+### 使用方法：
+1. `git clone https://github.com/thomastieyi/turtlePaints.git`
+2. `cd turtlePaints`
+3. `python init.py`
